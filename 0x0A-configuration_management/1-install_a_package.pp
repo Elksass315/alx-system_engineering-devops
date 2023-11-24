cc5df795e48786fb5ef04744e7978437f1f76710
Elksass315 <qsasomar@gmail.com>
2023-11-24 19:24:25 +0200

    FIX

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,13 +1,5 @@
-class flask {
-  package { 'python3-pip':
-    ensure => installed,
-  }
+# install flask
 
   exec { 'install-flask':
     command     => '/usr/bin/pip3 install flask==2.1.0',
-    require     => Package['python3-pip'],
-    unless      => '/usr/bin/pip3 show flask | /bin/grep -q "Version: 2.1.0"',
   }
-}
-
-include flask
