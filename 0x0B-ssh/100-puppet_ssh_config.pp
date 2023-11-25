a208a06bb77c0f5504e15f60fe743aa7a8331679
Elksass315 <qsasomar@gmail.com>
2023-11-25 15:56:59 +0200

    w/ pupet

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+# Puppet scrip to make changes to our configuration file. 
+file_line { 'Turn off passwd':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+}
+
+file_line { 'identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school',
+}
