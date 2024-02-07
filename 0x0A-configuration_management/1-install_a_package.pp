2595d334c1ac00c5d77e2aba4f46ac54222252c2
Elksass <qsasomar@gmail.com>
2024-02-07 19:42:13 +0200

    try

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,5 @@
-#!/usr/bin/pup
-# install flask
-exec { 'install python packages':
-     command   => 'pip3 install flask flask_restful apiai',
-     path => ['/usr/bin/'],
-     unless  => '/usr/bin/test -f /usr/local/lib/python3.4/dist-packages/flask/app.py'
-  }
+# Install flask
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
+}
