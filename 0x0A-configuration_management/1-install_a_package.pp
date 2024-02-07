74188b33f5f13b2e7da0e12278dc2cd437dc1274
Elksass <qsasomar@gmail.com>
2024-02-07 19:53:31 +0200

    try

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -5,9 +5,15 @@ package { 'python3-pip':
   ensure => installed,
 }
 
-# Use pip3 to install Flask
+# Use pip3 to install Flask and Werkzeug
 package { 'flask':
   ensure   => '2.1.0',
+  provider => 'pip3',
+  require  => Package['python3-pip'],
+}
+
+package { 'werkzeug':
+  ensure   => '2.0.2',
   provider => 'pip3',
   require  => Package['python3-pip'],
 }
