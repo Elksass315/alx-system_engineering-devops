9c8d8296242437edd93355ef8f1d244d3e3287fe
Elksass <qsasomar@gmail.com>
2024-03-05 22:39:19 +0200

    try

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,7 +9,8 @@ def number_of_subscribers(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {"User-Agent": "Mozilla/5.0"}
     res = requests.get(url, headers=headers, allow_redirects=False)
-    try:
-        return req.json()['data']['subscribers']
-    except:
+
+    if res.status_code == 200:
+        return res.json()['data']['subscribers']
+    else:
         return 0
