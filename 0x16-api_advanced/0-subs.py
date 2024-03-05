8a036df694ce6bdaed5134734dc561643bf74d21
Elksass <qsasomar@gmail.com>
2024-03-05 22:40:46 +0200

    try

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
 """
-0. How many subs? reddit API
+Script that queries subscribers on a given Reddit subreddit.
 """
+
 import requests
 
-def number_of_subscribers(subreddit):
-    """How many subs? reddit """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "Mozilla/5.0"}
-    res = requests.get(url, headers=headers, allow_redirects=False)
 
-    if res.status_code == 200:
-        return res.json()['data']['subscribers']
+def number_of_subscribers(subreddit):
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
     else:
         return 0
