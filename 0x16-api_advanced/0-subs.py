023cb14e620816bb0ffe9835dee6d3c08e8c5b05
Elksass315 <qsasomar@gmail.com>
2024-03-05 22:26:17 +0200

    Task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+0. How many subs? reddit API
+"""
+import requests
+
+def number_of_subscribers(subreddit):
+    """
+        How many subs? reddit API
+        :arg
+            subreddit: username
+
+        :return number of subscribers
+    """
+
+    req = requests.get(f"https://www.reddit.com/r/{subreddit}/about.json", allow_redirects=False)
+
+    try:
+        return req.json()['data']['subscribers']
+    except:
+        return 0
